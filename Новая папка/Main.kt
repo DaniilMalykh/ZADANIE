ea1559f928d69eacabc8c4c1f803d31299e7920c
DaniilMalykh <56538397+DaniilMalykh@users.noreply.github.com>
2022-03-17 19:39:52 +0800

    Add files via upload

---
+++ Новая папка/Main.kt
@@ -0,0 +1,42 @@
+import java.io.BufferedReader
+import java.io.File
+
+fun main() {
+
+
+    val readingStream = BufferedReader({}.javaClass.getResource("Text.txt").openStream().buffered().reader())
+    val dictionary = mutableMapOf<String, HashMap<Int, String>>()
+    var number = 1
+    var readingRoles = false
+
+    readingStream.forEachLine { line ->
+        if (line.contains("roles")) {
+            readingRoles = true
+        } else if (line.contains("textLines")) {
+            readingRoles = false
+        } else {
+            if (readingRoles) {
+                dictionary[line] = HashMap()
+            } else {
+
+                val role = line.split(':') [0]
+                val roleWord = line.substring(line.indexOf(':') + 1).trim()
+                val rolesWords = dictionary[role]
+                rolesWords?.let { it -> it[number] = roleWord }
+                number++
+            }
+        }
+    }
+    File("./out/production/roles/outText.txt").bufferedWriter().use { buffWriter ->
+        for (role in dictionary.keys) {
+            buffWriter.write("${role}:\n")
+            val roleWordss = dictionary[role]
+            roleWordss?.let { roleWordss ->
+                for (phraseKey in roleWordss.keys) {
+                    buffWriter.write("${phraseKey} ${roleWordss[phraseKey]}\n")
+                }
+            }
+            buffWriter.newLine()
+        }
+    }
+}
